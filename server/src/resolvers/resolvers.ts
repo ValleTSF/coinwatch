a69c0100d4e3a1b937518d48302d157ed902ed59
Rudhag <Malcolm.Rudhag@kgk.se>
2021-11-08 16:39:34 +0100

    symbols and assets

--- server/src/resolvers/resolvers.ts
+++ server/src/resolvers/resolvers.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import endpoints from "../endpoints";
 import { books, mainCards } from "../index";
 
 export const resolvers = {
@@ -9,6 +10,35 @@ export const resolvers = {
       return axios
         .get("https://dog.ceo/api/breeds/image/random")
         .then(({ data }) => data);
+    },
+    assets: (parent: any, args: any) => {
+      const { searchString } = args;
+      return axios
+        .get(endpoints.getAssets(searchString), {
+          headers: {
+            "X-CoinApi-Key": "94C3CE84-2FF6-4795-9B28-97325DE713C5",
+          },
+        })
+        .then(({ data }) => {
+          return data;
+        });
+    },
+    symbols: (parent: any, args: any) => {
+      const { searchString } = args;
+      console.log("args", args);
+      console.log("searchString", searchString);
+      return axios
+        .get(
+          `https://rest.coinapi.io/v1/symbols?filter_symbol_id=${searchString}`,
+          {
+            headers: {
+              "X-CoinApi-Key": "94C3CE84-2FF6-4795-9B28-97325DE713C5",
+            },
+          }
+        )
+        .then(({ data }) => {
+          return data;
+        });
     },
   },
 };
--- server/src/typeDefs/typesDefs.ts
+++ server/src/typeDefs/typesDefs.ts
@@ -16,10 +16,52 @@ export const typeDefs = gql`
     status: String
   }
 
+  type Symbol {
+    symbol_id: ID!
+    exchange_id: String!
+    symbol_type: String!
+    asset_id_base: String
+    asset_id_quote: String
+    option_type_is_call: Boolean
+    option_strike_price: Float
+    option_contract_unit: Float
+    option_exercise_style: String
+    option_expiration_time: String
+    data_end: String
+    data_quote_end: String
+    data_trade_end: String
+    symbol_id_exchange: String
+    asset_id_base_exchange: String
+    asset_id_quote_exchange: String
+    image: String
+  }
+
+  type Asset {
+    asset_id: ID!
+    name: String!
+    type_is_crypto: Int!
+    data_quote_start: String
+    data_quote_end: String
+    data_orderbook_start: String
+    data_orderbook_end: String
+    data_trade_start: String
+    data_trade_end: String
+    data_symbols_count: Int!
+    volume_1hrs_usd: Float
+    volume_1day_usd: Float
+    volume_1mth_usd: Float
+    price_usd: Float
+    id_icon: String
+    data_start: String
+    data_end: String
+  }
+
   type Query {
     books: [Book]
     mainCards: [MainCard]
     dog: Dog
+    symbols(searchString: String!): [Symbol]
+    assets(searchString: String!): [Asset]
   }
 `;
 
