d69bcc1767733934aac460975650143e248fed24
Rudhag <Malcolm.Rudhag@kgk.se>
2021-11-27 09:48:16 +0100

    assets merged with icons

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from "apollo-server";
 import axios, { AxiosResponse } from "axios";
 import endpoints from "./endpoints";
-import { Asset as AssetInterface } from "./models";
+import { Asset as AssetInterface, AssetIcon, AssetMerged } from "./models";
 import { Asset, Query } from "./resolvers";
 import { typeDefs } from "./typeDefs";
 
@@ -9,24 +9,44 @@ require("dotenv").config();
 
 const headers = { "X-CoinAPI-Key": process.env.COINAPI_API_KEY! };
 
-const getAssets = async () => {
-  return await axios.get(endpoints.getAllAssets, { headers });
+const getAssets = () => {
+  const mergedList: AssetMerged[] = [];
+  axios
+    .get(endpoints.getAllAssets, { headers })
+    .then(({ data: assetList }: AxiosResponse<AssetInterface[]>) => {
+      axios
+        .get(endpoints.getAllAssetIcons, { headers })
+        .then(({ data: assetIconList }: AxiosResponse<AssetIcon[]>) => {
+          assetList.forEach((asset) => {
+            const assetIcon = assetIconList.find(
+              (icon) => icon.asset_id === asset.asset_id
+            );
+            if (assetIcon) {
+              const { url } = assetIcon;
+              mergedList.push({ ...asset, image: url });
+            }
+          });
+        })
+        .catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error));
+
+  return mergedList;
 };
 
-getAssets().then(({ data }: AxiosResponse<AssetInterface[]>) => {
-  const cryptoCurrencies = data.filter((asset) => asset.type_is_crypto === 1);
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers: {
-      Query,
-      Asset,
-    },
-    context: {
-      assets: cryptoCurrencies,
-    },
-  });
+const assets = getAssets();
+const cryptoCurrencies = assets.filter((asset) => asset.type_is_crypto === 1);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query,
+    Asset,
+  },
+  context: {
+    assets: cryptoCurrencies,
+  },
+});
 
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  });
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
--- server/src/models/models.ts
+++ server/src/models/models.ts
@@ -16,5 +16,30 @@ export interface Asset {
   id_icon: string;
   data_start: string;
   data_end: string;
-  asset_icon: string;
+}
+
+export interface AssetIcon {
+  asset_id: string;
+  url: string;
+}
+
+export interface AssetMerged {
+  asset_id: string;
+  name: string;
+  type_is_crypto: number;
+  data_quote_start: string;
+  data_quote_end: string;
+  data_orderbook_start: string;
+  data_orderbook_end: string;
+  data_trade_start: string;
+  data_trade_end: string;
+  data_symbols_count: number;
+  volume_1hrs_usd: number;
+  volume_1day_usd: number;
+  volume_1mth_usd: number;
+  price_usd: number;
+  id_icon: string;
+  data_start: string;
+  data_end: string;
+  image: string;
 }
